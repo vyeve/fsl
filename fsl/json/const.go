454ab781c3b14a828bcd628a04e0ef4842be46a8
vyeve <xobpaxa@ukr.net>
2022-07-23 15:08:43 +0300

    added constants

---
+++ fsl/json/const.go
@@ -0,0 +1,22 @@
+package json
+
+const (
+	initFnKey     = "init"
+	createFnKey   = "create"
+	deleteFnKey   = "delete"
+	updateFnKey   = "update"
+	addFnKey      = "add"
+	subtractFnKey = "subtract"
+	multiplyFnKey = "multiply"
+	divideFnKey   = "divide"
+	printFnKey    = "print"
+)
+
+const (
+	cmdKey           = "cmd"
+	idKey            = "id"
+	valueKey         = "value"
+	operand1Key      = "operand1"
+	operand2Key      = "operand2"
+	undefinedKeyword = "undefined"
+)
